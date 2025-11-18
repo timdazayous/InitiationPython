ec9653b8ac611a06c670b44fcd3000c15d6ee09b
timdazayous <tim.dazayous@gmail.com>
2025-11-18 10:18:34 +0100

    commentaire script.py

--- script.py
+++ script.py
@@ -4,7 +4,8 @@ PATH = r'.venv'
 Nfiles = 0
 Ndirs = 0
 
-
+# oswalk(path) parcourt recursivement le dossier .venv
+# root = chemin du dossier en cours / dirnames = liste des sous dossiers dans ce dossier / filenames = liste des fichiers dans ce dossier
 for root, dirnames, filenames in oswalk(PATH):
     Ndirs = Ndirs + len(dirnames)
     Nfiles += len(filenames)
